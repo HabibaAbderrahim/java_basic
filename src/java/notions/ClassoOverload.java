27ebb834956f27ee0a7542b0ccec5543a6c7e97e
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-10 19:09:51 +0100

    overloading

---
+++ src/java/notions/ClassoOverload.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class ClassoOverload {
+
+    //method with the same name and different params
+
+    public static int add (int a , int b){
+        return a+b;
+    }
+
+   /* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   No same parameters even though the return is different
+    public static double add(int a , int b){
+        return a+b;
+    }*/
+
+    public static double add(double a , double b){
+        return a+b;
+    }
+}
