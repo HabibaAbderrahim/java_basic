ca9b982b66cb02c752f0cfe76571faf7286d9c49
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-20 18:35:55 +0100

    inhern

--- src/inher/Polegon.java
+++ src/inher/Polegon.java
@@ -1,2 +1,27 @@
-package inher;public class prop {
+package inher;
+
+public class Polegon {
+
+    //Those variables will be subclassed to other classes that inhert the main Class so they ca't be private so we use protected
+    protected int height ;
+    protected int weight ;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+
+
 }
--- src/inher/Triangle.java
+++ src/inher/Triangle.java
@@ -1,2 +1,25 @@
-package inher;public class Triangle {
+package inher;
+
+public class Triangle extends Polegon {
+
+    public int getArea(){
+
+        //Direct use since it inheret properties of the main Class
+        return height*weight;
+    }
+
+    public static class Test {
+
+        public static void main(String[] args) {
+            Triangle t = new Triangle();
+            t.setHeight(10);
+            t.setWeight(10);
+            int area = t.getArea();
+
+            System.out.println("Triangle Area that inhert the height and weight is= " +area);
+        }
+
+
+
+    }
 }
