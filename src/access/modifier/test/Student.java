b7e9ccd877bc25539532e45bc42b916007520cd2
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-20 18:36:29 +0100

    Acess modifiers

--- src/access/modifier/test/Student.java
+++ src/access/modifier/test/Student.java
@@ -1,2 +1,15 @@
-package access.modifier.test;public class Student {
+package access.modifier.test;
+
+public class Student {
+
+    //No modifer : let's see if it's accesable outside the package
+    //ERROR CAN't not be acceable outside the package
+    String adresse ;
+    // public : inside and outside
+    public String name ;
+    //private : best practice only visible
+    private int age ;
+
+
+
 }
--- src/access/modifier/test/Test.java
+++ src/access/modifier/test/Test.java
@@ -1,2 +1,11 @@
-package access.modifier.test;public class Test {
+package access.modifier.test;
+
+public class Test {
+    public static void main(String[] args) {
+        //Acess private value in same package
+        Student s = new Student();
+        //publice:s.name ---> Accessable inside and outside
+        //No modifier :s.adresse ---> Accable inside the same package outside NO
+        //Private : s.age ---> NOT accable even inside the same package only inside the Class
+    }
 }
